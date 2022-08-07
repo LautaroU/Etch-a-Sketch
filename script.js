cb1730ac9f056054af91cff289807ff5f9b20375
lautaro <lmontes54321@gmail.com>
2022-08-07 02:49:00 -0300

    POR FIN LPM

--- script.js
+++ script.js
@@ -1,57 +1,61 @@
 
 
-
-
-
-
-
-
-
 const divpa = document.querySelector('div');
 divpa.classList.add('father');
+const hijo1 = document.createElement('div');
+hijo1.classList.add('hijo1');
+divpa.appendChild(hijo1);
 
-for (i = 0; i < 16; i ++) {
+const hijo2 = document.createElement('div');
+hijo2.classList.add('hijo2');
+divpa.appendChild(hijo2);
 
+
+const btn = document.createElement('button');
+hijo1.appendChild(btn);
+btn.innerHTML = "Tamaño";
+btn.addEventListener('click', () => {
+    let numeros = prompt("Ingrese el tamaño para dibujar ej.(64x64, 16x16 etc.)" )
+    let division = (500/numeros) ;
+    division.toString();
+
+    let cantidad = numeros*numeros;
     
-    const fila = document.createElement('div');
-    fila.setAttribute('style', 'width:832px; height:29px; display:flex;')
-    fila.classList.add('fila');
-    divpa.appendChild(fila);
     
     
-    for (j = 0 ; j < 16; j ++) {
-    if (i === 11) {
-        
-        const square = document.createElement('div');
-        square.classList.add('square');
-        square.setAttribute('id', "once" + `${i}` + j)
-        fila.appendChild(square);
-        
-    }else{
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.setAttribute('id', `${i}` + j )
-    fila.appendChild(square); 
+    const cuadrado = document.createElement('div');
+    cuadrado.classList.add('cuadrado');
+    hijo2.appendChild(cuadrado);
+    
+    
+
+    for (i = 1; i <= cantidad; i ++ ) {
+    const pruebaDiv = document.createElement('div');
+    pruebaDiv.classList.add('aa');
+    pruebaDiv.setAttribute('style', `width:${division}px; height:${division}px; background-color:red;`)
+    pruebaDiv.setAttribute('id', i);
+    cuadrado.appendChild(pruebaDiv);
     }
-    }
+    const divs = document.querySelectorAll('div');
     
-    
-    
-  
-   
-    
-}
-
-
-const divs = document.querySelectorAll('div');
-    
-divs.forEach((div) => div.addEventListener('mouseover', function(e){
+    divs.forEach((div) => div.addEventListener('mouseover', function(e){
     e.stopPropagation();
     const nEw = document.querySelector(`div[id="${e.target.id}"]`)
     if(!nEw) return;
-    nEw.classList.remove('square')
+    nEw.classList.remove('aa')
+    
+    nEw.setAttribute('style', `width:${division}px; height:${division}px; background-color:yellow;`)
     nEw.classList.add('newSquare')
+    
+
 }))
+})
+
+
+
+
+
+
     
 
    
